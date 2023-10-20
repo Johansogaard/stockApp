1d5830e8452380cdaa5355e74eb339424e8fb110
Johansogaard <Johansogaardjorgensen@gmail.com>
2023-10-20 17:26:12 +0200

    working on authentication JJ

--- app/src/main/java/com/example/stockapp/MainActivity.kt
+++ app/src/main/java/com/example/stockapp/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.example.stockapp
 
+import AuthenticationManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.remember
+import com.example.stockapp.ui.theme.StockAppTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           MainNavHost()
+            StockAppTheme {
+
+
+                MainNavHost()
+            }
         }
 
     }
--- app/src/main/java/com/example/stockapp/Screen.kt
+++ app/src/main/java/com/example/stockapp/Screen.kt
@@ -2,7 +2,7 @@ package com.example.stockapp
 
 sealed class Screen(val route: String) {
     object IntroScreen: Screen("intro_screen")
-    object LoginScreen: Screen("login_screen")
+    object LoginScreen: Screen("chose_signup_screen")
     object PortfolioScreen: Screen("portfolio_screen")
 
     fun withArgs(vararg args: String): String{
