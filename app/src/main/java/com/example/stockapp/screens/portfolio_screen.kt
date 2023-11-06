2e69029958136d61346c0d2c9b45a0e1cfdab344
mikkel <mtschwarz9@gmail.com>
2023-11-06 11:07:47 +0100

    Portfolio screen updated

--- app/src/main/java/com/example/stockapp/screens/portfolio_screen.kt
+++ app/src/main/java/com/example/stockapp/screens/portfolio_screen.kt
@@ -1,5 +1,6 @@
 package com.example.stockapp.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,8 +17,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.stockapp.data.Screen
@@ -25,7 +29,10 @@ import com.example.stockapp.authentication.EmailAuthManager
 
 @Composable
 fun PortfolioScreen(navController: NavController) {
-    Box(modifier = Modifier.fillMaxSize())
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.White)
+    )
     {
         PortfolioLayout(navController)
 
@@ -33,34 +40,41 @@ fun PortfolioScreen(navController: NavController) {
 
 }
 @Composable
-fun PortfolioLayout(navController: NavController)
-{
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(14.dp)
-    )
-    {
-        Row( modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween, // Align children to the start and end of the Row
-            verticalAlignment = Alignment.CenterVertically) {
-            Text(text = "Overview")
-            IconButton(
-                onClick = {
-                          EmailAuthManager.signOut()
-                    navController.navigate(Screen.IntroScreen.route)
-                },
+fun PortfolioLayout(navController: NavController) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier.fillMaxSize().padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "portfolio",
+                fontSize = 20.sp,
+                fontWeight= FontWeight.Bold
+            )
 
-
+            Column(
+                modifier = Modifier.fillMaxWidth().padding(20.dp),
+                horizontalAlignment = Alignment.Start
             ) {
-                Icon(
-                    imageVector = Icons.Rounded.ExitToApp,
-                    contentDescription = "exit app icon",
-                    tint = Color.Black // You can change the icon color here
+                Text(
+                    text = "Overview",
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold
                 )
+
+                Text(
+                    text = "Equity capital (DKK)",
+                    fontSize = 10.sp
+                )
+                Text(text = "122.388",
+                    fontSize = 25.sp,
+                    fontWeight = FontWeight.Bold)
             }
         }
-        }
+
     }
+}
+
+
 
 @Preview(showBackground = true)
 @Composable
