f1d253da65c4ddad3fd54610c8b74848f1317f95
Johan Sogaard <Johansogaardjorgensen@gmail.com>
2023-12-22 00:52:55 +0100

    working on rules to login jj

--- app/src/main/kotlin/com/example/stockapp/screens/login_screen.kt
+++ app/src/main/kotlin/com/example/stockapp/screens/login_screen.kt
@@ -1,18 +1,11 @@
 package com.example.stockapp.screens
 
-import android.content.Intent
 import android.widget.Toast
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -23,14 +16,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.stockapp.R
 import com.example.stockapp.authentication.EmailAuthManager
 import com.example.stockapp.data.Screen
 import com.example.stockapp.ui.theme.ClickableText
 import com.example.stockapp.ui.theme.CustomButton
 import com.example.stockapp.ui.theme.CustomTextField
-import com.example.stockapp.ui.theme.Purple40
 import com.example.stockapp.viewModels.UserViewModel
-import com.example.stockapp.R
 
 @Composable
 fun LoginScreen(navController: NavController, userViewModel: UserViewModel)
@@ -80,19 +72,24 @@ fun LoginLayout(navController: NavController, userViewModel: UserViewModel)
         )
 
         Spacer(modifier = Modifier.height(16.dp))
-        CustomButton(onClick = {                    navController.navigate(Screen.PortfolioScreen.route)
-/*
-            EmailAuthManager.signIn(username.value.text, password.value.text) { isSuccess, errorMessage ->
-                if (userViewModel.state.value.isLoggedIn true) {
-                    navController.navigate(Screen.PortfolioScreen.route)
+        CustomButton(onClick = {
+            val uName = username.value.text
+            val pWord = password.value.text
+            if (uName.length == 0 || pWord.length == 0) {
+                Toast.makeText(context, "empty username or password", Toast.LENGTH_SHORT).show()
+            } else {
+                EmailAuthManager.signIn(uName, pWord) { isSuccess, errorMessage ->
+                    if (userViewModel.state.value.isLoggedIn) {
+                        navController.navigate(Screen.PortfolioScreen.route)
+                    } else {
+                        val formattedErrorMessage = String.format(loginFailedMessage, errorMessage)
+                        Toast.makeText(context, formattedErrorMessage, Toast.LENGTH_SHORT).show()
+                        println("Login failed. Error message: $errorMessage")
+                    }
                 }
-                else {
-                    val formattedErrorMessage = String.format(loginFailedMessage, errorMessage)
-                    Toast.makeText(context, formattedErrorMessage, Toast.LENGTH_SHORT).show()
-                    println("Login failed. Error message: $errorMessage")
-                }
-            }*/
-        }, text = stringResource(R.string.common_login))
+            }
+        }
+        , text = stringResource(R.string.common_login))
         OrDivider()
 
 
