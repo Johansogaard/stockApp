2992e7b9d17eae94c9b2c6b90240e88d3c60d91a
LuucM <luucmeldgaard@outlook.com>
2024-01-07 11:53:07 +0100

    updates AGP to 8.2.1, found an issue with HttpOK that makes a need for retries within StockApi, and is uncorrectable due to the nature of HttpOK with kotlin. Can be found as issue #907 on the HttpOK Github page.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.0" apply false
+    id("com.android.application") version "8.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.21" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.21" apply false
@@ -15,7 +15,7 @@ buildscript {
         gradlePluginPortal()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("com.android.tools.build:gradle:8.2.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21") // Kotlin Gradle Plugin
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.50") // Hilt Gradle Plugin
         classpath("com.google.gms:google-services:4.4.0")
